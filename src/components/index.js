fbdcf96b9b772ee5df9d31ef39bab5a5978aa5f0
MenaraWas <54022002+MenaraWas@users.noreply.github.com>
2024-05-12 15:02:11 +0700

    First Commit

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import Business from "./business";
+import Button from "./button";
+import CardDeal from "./cardDeal";
+import Client from "./clients";
+import CTA from "./cta";
+import FeedbackCard from "./feedbackCard";
+import Footer from "./footer";
+import GetStarted from "./GetStarted";
+import Hero from "./hero";
+import Stats from "./stats";
+import Testimonials from "./testimonials";
+
+export { 
+    Navbar, 
+    Billing,
+    Business,
+    Button,
+    CardDeal,
+    Client, 
+    CTA, 
+    FeedbackCard, 
+    Footer,
+    GetStarted,
+    Hero,
+    Stats,
+    Testimonials
+};
