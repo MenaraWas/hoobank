6de7bd84f0b66e4bd0a80d6954acbbed8b5c875d
MenaraWas <54022002+MenaraWas@users.noreply.github.com>
2024-05-12 17:49:20 +0700

    debug

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import styles from "./style";
 
-import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from "./components";
+import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, CTA, Footer } from "./components";
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
@@ -23,7 +23,6 @@ const App = () => (
       <Billing />
       <CardDeal />
       <Testimonials />
-      <Clients />
       <CTA />
       <Footer />
       </div>
